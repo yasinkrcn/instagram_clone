22cd359f16cd605df546322a2e8078f5c38ac02a
yasinkrcn <23yasin1999@gmail.com>
2022-10-17 19:42:39 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:instagram_clone/core/keys/global_key.dart';
 import 'package:instagram_clone/core/utils/route/router.dart';
 import 'package:instagram_clone/feature/auth/view/pages/create_account.dart';
+import 'package:instagram_clone/feature/auth/view/pages/login_page.dart';
 import 'package:instagram_clone/feature/auth/view/pages/start_page.dart';
 import 'package:instagram_clone/feature/home/view/page/home_page.dart';
 
@@ -22,7 +23,7 @@ class InstagramClone extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }

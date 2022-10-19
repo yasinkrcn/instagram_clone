8c3eb6db8afd96930ff4889284145b6c3d41418f
yasinkrcn <23yasin1999@gmail.com>
2022-10-20 01:03:57 +0300

    new assets add

--- lib/core/constants/assets_paths.dart
+++ lib/core/constants/assets_paths.dart
@@ -10,17 +10,38 @@ class AssetsPath {
   final String addSVG = 'assets/svg/add.svg';
   final String commentSVG = 'assets/svg/comment.svg';
   final String heartSVG = 'assets/svg/heart.svg';
-  final String homeSVG = 'assets/svg/home.svg';
   final String logoSVG = 'assets/svg/logo.svg';
-  final String messageSVG = 'assets/svg/message.svg';
-  final String reelsSVG = 'assets/svg/reels.svg';
   final String saveSVG = 'assets/svg/save.svg';
-  final String searchSVG = 'assets/svg/search.svg';
   final String sendSVG = 'assets/svg/send.svg';
-  final String shoppingSVG = 'assets/svg/shopping.svg';
   final String menuhamburgerSVG = 'assets/svg/menu_hamburger.svg';
   final String profile1SVG = 'assets/svg/profile1.svg';
   final String profile2SVG = 'assets/svg/profile2.svg';
   final String obscureSVG = 'assets/svg/obscure.svg';
   final String facebookSVG = 'assets/svg/facebook_logo.svg';
+
+  //!BOTTOM NAV BAR
+
+  //ACTIVE
+  final String activeHomeSVG =
+      'assets/svg/bottom_nav_bar/active/active_home.svg';
+  final String activeSearchSVG =
+      'assets/svg/bottom_nav_bar/active/active_search.svg';
+  final String activeReelsSVG =
+      'assets/svg/bottom_nav_bar/active/active_reels.svg';
+  final String activeShopSVG =
+      'assets/svg/bottom_nav_bar/active/active_shop.svg';
+  final String activeProfileSVG =
+      'assets/svg/bottom_nav_bar/active/active_profile.svg';
+
+  //DEACTIVE
+  final String deactiveHomeSVG =
+      'assets/svg/bottom_nav_bar/deactive/deactive_home.svg';
+  final String deactiveSearchSVG =
+      'assets/svg/bottom_nav_bar/deactive/deactive_search.svg';
+  final String deactiveReelsSVG =
+      'assets/svg/bottom_nav_bar/deactive/deactive_reels.svg';
+  final String deactiveShopSVG =
+      'assets/svg/bottom_nav_bar/deactive/deactive_shop.svg';
+  final String deactiveProfileSVG =
+      'assets/svg/bottom_nav_bar/deactive/deactive_profile.svg';
 }

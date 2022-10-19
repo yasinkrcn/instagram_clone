48a9aa985f807f69e10fd394298de8467373d94f
yasinkrcn <23yasin1999@gmail.com>
2022-10-20 01:03:50 +0300

    service locator and provider add

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,27 @@
+import 'package:instagram_clone/core/constants/main_provider_list%20copy.dart';
+import 'package:provider/provider.dart';
+
+import '/core/init/injection_container.dart' as locator;
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/core/keys/global_key.dart';
 import 'package:instagram_clone/core/utils/route/router.dart';
-import 'package:instagram_clone/feature/auth/view/pages/create_account.dart';
+import 'package:instagram_clone/feature/auth/view/pages/create_account_page.dart';
 import 'package:instagram_clone/feature/auth/view/pages/login_page.dart';
 import 'package:instagram_clone/feature/auth/view/pages/start_page.dart';
 import 'package:instagram_clone/feature/home/view/page/home_page.dart';
 import 'package:instagram_clone/feature/profile/view/page/profile_page.dart';
 
-void main() {
-  runApp(const InstagramClone());
+import 'feature/bottom_navigation_bar/view/page/bottom_navigation_page.dart';
+
+void main()async {
+    await locator.init();
+  runApp(
+    
+    MultiProvider(
+
+      providers: MainProviderList.getMainProviderList(),
+      
+      child: const InstagramClone()));
 }
 
 class InstagramClone extends StatelessWidget {
@@ -24,7 +37,7 @@ class InstagramClone extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ProfilePage(),
+      home: const BottomNavigationPage(),
     );
   }
 }

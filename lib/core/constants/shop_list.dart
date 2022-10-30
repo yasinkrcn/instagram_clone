7f1752ea3cf80980fcb460db3b3362d83f1f2771
yasinkrcn <23yasin1999@gmail.com>
2022-10-31 01:09:11 +0300

    creating shop list

---
+++ lib/core/constants/shop_list.dart
@@ -0,0 +1,11 @@
+import 'package:instagram_clone/core/constants/assets_paths.dart';
+
+ List shop = [
+  AssetsPath().shop1PNG,
+  AssetsPath().shop2PNG,
+  AssetsPath().shop3PNG,
+  AssetsPath().shop4PNG,
+  AssetsPath().shop5PNG,
+  AssetsPath().shop6PNG,
+  AssetsPath().shop7PNG,
+];
